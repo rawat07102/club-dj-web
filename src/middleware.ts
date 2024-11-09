7daecc6fe388a77633f50d460c61afb2ee5651bf
Ritik Rawat <ritikrawat0710@gmail.com>
2024-11-09 13:00:43 +0530

    make middleware protect routes based on the string it starts with

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,16 @@ import { NextRequest, NextResponse } from "next/server"
 import { cookies } from "next/headers"
 
 // 1. Specify protected and public routes
-const protectedRoutes = ["/dashboard"]
+const protectedRoutes = ["/dashboard", "/clubs"]
 const publicRoutes = ["/auth/login", "/auth/signup"]
 
 export default async function middleware(req: NextRequest) {
     // 2. Check if the current route is protected or public
     const path = req.nextUrl.pathname
-    const isProtectedRoute = protectedRoutes.includes(path)
+    //const isProtectedRoute = protectedRoutes.includes(path)
+    const isProtectedRoute = protectedRoutes.some((route) =>
+        path.startsWith(route)
+    )
     const isPublicRoute = publicRoutes.includes(path)
 
     // 3. Decrypt the session from the cookie
@@ -23,11 +26,7 @@ export default async function middleware(req: NextRequest) {
     }
 
     // 6. Redirect to /dashboard if the user is authenticated
-    if (
-        isPublicRoute &&
-        isAuthenticated &&
-        !req.nextUrl.pathname.startsWith("/dashboard")
-    ) {
+    if (isPublicRoute && isAuthenticated && !isProtectedRoute) {
         return NextResponse.redirect(new URL("/dashboard", req.nextUrl))
     }
 
